2b252d22493f1c1946229a8e726b50cb58e59f06
alex <alex3889660@gmail.com>
2021-08-06 01:19:38 +0800

    push none

--- middleware/passport-setup.js
+++ middleware/passport-setup.js
@@ -1,22 +1,20 @@
-const passport = require('passport');
-const GoogleStratery = require('passport-google-token').Strategy;
-const key = require('../config/key');
+const passport = require("passport");
+const GoogleStratery = require("passport-google-token").Strategy;
+const key = require("../config/key");
 
-
-passport.use(new GoogleStratery({
-  clientID: key.google.clientID,
-  clientSecret: key.google.clientSecret,
-}, async (accessToken, refreshToken, profile, cb) => {
-    const defaultMember = {
-      name: `${profile.name.givenName}${profile.name.familyName}`,
-      email: profile.emails[0].value,
-      image: profile._json.picture,
+passport.use(
+  new GoogleStratery(
+    {
+      clientID: key.google.clientID,
+      clientSecret: key.google.clientSecret,
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      const defaultMember = {
+        name: `${profile.name.givenName}${profile.name.familyName}`,
+        email: profile.emails[0].value,
+        image: profile._json.picture,
+      };
+      cb(null, defaultMember);
     }
-    cb(null, defaultMember)
-  }
-));
-
-
-
-
-
+  )
+);
